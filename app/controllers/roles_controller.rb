12decd9c196f350978b3fa9b42eac9cf20010e94
yamiacat <yamiacat@gmail.com>
2017-07-18 15:27:26 +0100

    Add clean up of roles_controller to display only required fields

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,10 +2,9 @@ class RolesController < ApplicationController
   def index
     roles = Actor.find( params[:actor_id]).roles
     render :json => roles.as_json(
-      # {only: :character}
-        { include:
-          {movie: {only: [:title, :year]}}
-          }
+    {except:[:created_at, :updated_at, :movie_id, :actor_id, :id],
+    include:
+          {movie: {only: [:title, :year]}}}
         )
 
   end
